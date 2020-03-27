8af492b5db7ab17e64a2170e3f50e9a26437e6f1
szpatrik13 <55582396+szpatrik13@users.noreply.github.com>
2020-03-27 08:26:12 +0100

    Add files via upload

---
+++ Futar/Futar/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("fuvar.csv");
+            List<Ut> Utak = new List<Ut>();
+            string fejlec = reader.ReadLine();
+            string sor;
+            while ((sor = reader.ReadLine()) != null)
+            {
+                Utak.Add(new Ut(sor));
+            }
+            reader.Close();
+
+            Console.WriteLine($"3. feladat: {Utak.Count} fuvar");
+
+            Console.Write($"4. feladat: ");
+            int db = 0;
+            float bevetel = 0;
+            foreach (var item in Utak)
+            {
+                if (item.TaxiId == 6185)
+                {
+                    db++;
+                    bevetel = bevetel + item.Viteldij + item.Borravalo;
+                }
+            }
+
+            Console.WriteLine($"{db} fuvar alatt {bevetel} $");
+
+            Console.WriteLine($"5. feladat: ");
+            Dictionary<string, int> szamlaloFizModonkent = new Dictionary<string, int>();
+            foreach (var item in Utak)
+            {
+                if (szamlaloFizModonkent.ContainsKey(item.FizetesModja))
+                {
+                    szamlaloFizModonkent[item.FizetesModja]++;
+                }
+                else
+                {
+                    szamlaloFizModonkent.Add(item.FizetesModja, 1);
+                }
+            }
+
+            foreach (var item in szamlaloFizModonkent)
+            {
+                Console.WriteLine($"\t{item.Key}: {item.Value}");
+            }
+
+            Console.Write($"6. feladat: ");
+            float osszkm = 0;
+            foreach (var item in Utak)
+            {
+                osszkm += item.Tavolsag * (float)1.6;
+            }
+            Console.WriteLine($"{osszkm:F2} km");
+
+            Console.WriteLine($"7. feladat: ");
+
+            Ut maxFuvar = null;
+
+
+            maxFuvar = Utak.OrderByDescending(x => x.Idotartam).First();
+            Console.WriteLine($"\tA fuvar hossza: {maxFuvar.Idotartam} ");
+            Console.WriteLine($"\tTaxi azonosító: {maxFuvar.TaxiId}");
+            Console.WriteLine($"\tMegtett távolság: {maxFuvar.Tavolsag}");
+            Console.WriteLine($"\tViteldíj: {maxFuvar.Viteldij}");
+
+            Console.WriteLine("hibak.txt......");
+
+            StreamWriter writer = new StreamWriter("hibak.txt");
+            List<string> hibaLista = new List<string>();
+            writer.WriteLine(fejlec);
+            Utak = Utak.OrderBy(x => x.IndulasIdeje).ToList();
+            foreach (var item in Utak)
+            {
+                if ((item.Idotartam > 0 && item.Tavolsag == 0) && item.Viteldij > 0)
+                {
+                    writer.WriteLine($"{item.TaxiId};{item.IndulasIdeje};{item.Idotartam};{item.Tavolsag};" +
+                        $"{item.Viteldij};{item.Borravalo};{item.FizetesModja}");
+
+                }
+            }
+            writer.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Futar/Futar/ut.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Ut
+    {
+        public int TaxiId { get; set; }
+        public string IndulasIdeje { get; set; }
+        public int Idotartam { get; set; }
+        public float Tavolsag { get; set; }
+        public float Viteldij { get; set; }
+        public float Borravalo { get; set; }
+        public string FizetesModja { get; set; }
+
+        public Ut(string sor)
+        {
+            string[] mezok = sor.Split(';');
+            TaxiId = int.Parse(mezok[0]);
+            IndulasIdeje = mezok[1];
+            Idotartam = int.Parse(mezok[2]);
+            Tavolsag = float.Parse(mezok[3]);
+            Viteldij = float.Parse(mezok[4]);
+            Borravalo = float.Parse(mezok[5]);
+            FizetesModja = mezok[6];
+
+        }
+
+    }
+}
